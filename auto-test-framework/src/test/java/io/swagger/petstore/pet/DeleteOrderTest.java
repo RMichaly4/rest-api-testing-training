ea8ed155d5a117c0932ac2684790618c539ce805
RMichaly4 <100rm500@gmail.com>
2018-10-18 11:02:34 +0300

    Test Framework HW -- CreateOrderTest works! (2)

---
+++ auto-test-framework/src/test/java/io/swagger/petstore/pet/DeleteOrderTest.java
@@ -0,0 +1,41 @@
+package io.swagger.petstore.pet;
+
+import io.swagger.petstore.base.BaseTest;
+import io.swagger.petstore.client.PetClient;
+import io.swagger.petstore.model.Pet;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeleteOrderTest extends BaseTest {
+
+    private long petId;
+
+    @BeforeClass
+    public void createPet() {
+        Pet createdPet = PetClient.createPet(getGenericPetWithAllFields())
+                .assertThat().statusCode(200)
+                .extract().body().as(Pet.class);
+
+        assertThat(createdPet).isNotNull();
+        petId = createdPet.getId();
+    }
+
+    @Test
+    public void testDeletePet() {
+        String responseBody = PetClient.deletePet(petId)
+                .assertThat()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract().body().asString();
+
+        assertThat(responseBody)
+                .isNotNull()
+                .isEqualTo("");
+
+        PetClient.getPet(petId)
+                .assertThat().statusCode(404);
+    }
+}

ce85cd15cbae34b38d19d6c378b3c9a10900ec47
RMichaly4 <100rm500@gmail.com>
2018-11-05 10:04:03 +0300

    GetOrderValidationTest added

--- auto-test-framework/src/main/java/io/swagger/petstore/data/ResponseMessage.java
+++ auto-test-framework/src/main/java/io/swagger/petstore/data/ResponseMessage.java
@@ -5,4 +5,5 @@ public class ResponseMessage {
     public static final String ADDITIONAL_METADATA = "additionalMetadata: ";
     public static final String FILE_UPLOADED_TO = "\nFile uploaded to ./";
     public static final String PET_NOT_FOUND = "Pet not found";
+    public static final String ORDER_NOT_FOUND = "Order not found";
 }
